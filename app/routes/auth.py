fd3bdc5d5b3e6486871fb379049139ea51d4fb20
Annextt <davidnakaharagame@gmail.com>
2026-05-05 13:04:11 -0300

    auth

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -2,16 +2,10 @@ from fastapi import APIRouter, HTTPException, Request
 from app.schemas.auth import LoginInput, TokenOutput
 from app.database import users_collection
 from app.core.security import verificar_senha, criar_token
-from slowapi import Limiter
-from slowapi.util import get_remote_address
 
 router = APIRouter(prefix="/auth", tags=["Auth"])
 
-limiter = Limiter(key_func=get_remote_address)
-
-
 @router.post("/login", response_model=TokenOutput)
-@limiter.limit("5/minute")
 async def login(request: Request, data: LoginInput):
     print(f"[DEBUG] Login attempt: {data.email}")
     user = await users_collection.find_one({"email": data.email})
